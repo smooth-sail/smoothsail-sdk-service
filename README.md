779cbe9881f97dd2c622a345236578df262cde65
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-10-16 21:37:41 -0700

    Install and configure node-postgres

---
+++ README.md
@@ -0,0 +1,15 @@
+## Run locally
+
+- Create `.env` file with the following:
+
+```
+PGUSER=<username>
+PGHOST=localhost
+PGDATABASE=<databasename>
+PGPASSWORD=<yourpassword>
+PGPORT=5432
+```
+
+- if the name is between `<>` replace with your own credentials
+- if PGPORT is not set it will use default value of 5432
+- PGPASSWORD does not have to be set if you don't have a password
--- src/app.js
+++ src/app.js
@@ -2,21 +2,21 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import apiRoutes from './routes/crud-ff-routes';
+import { getClient } from './config/pg';
 
 const app = express();
-app.use(cors());  // this should be later replaced with whitelisted domains
+app.use(cors()); // this should be later replaced with whitelisted domains
 app.use(express.json());
 
+app.use('/api', apiRoutes);
+getClient();
 
-app.use("/api", apiRoutes);
+app.use('/', (req, res) => {
+  res.status(404).json({ error: 'no such route' });
+});
 
-app.use("/", (req, res) => {
-  res.status(404).json({ error: "no such route" });
-})
-
-const PORT = process.env.PORT;
-app.listen(PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`),
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
 export default app;
+
