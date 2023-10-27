39710330fd016ae40cd0af00dfa17c1436a443c2
Emily Olszewski <92195356+emilyoski@users.noreply.github.com>
2023-10-26 17:42:40 -0700

    utilizing flagCache

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import express from "express";
 import cors from "cors";
 import axios from "axios";
 // import apiRouter from "./routes/Api.routes";
-import { Flags } from "./models/flags";
+import FlagCache from "./cache/flagCache";
+import { Flag } from "./models/flag";
+import { handleUpdateNotification } from "./utils/flags";
 
 const app = express();
 
@@ -12,27 +14,30 @@ app.use(express.json());
 
 // initial fetch of flag data when application spun up
 const GET_ALL_FLAGS = "http://localhost:3000/api/sdk/flags";
-let flagData;
+
 (async () => {
   const { data } = await axios.get(GET_ALL_FLAGS);
-  flagData = new Flags(data.payload);
-  console.log("flag data: ", flagData.formattedForSDK());
+  for (let flag in data.payload) {
+    FlagCache[flag] = new Flag(data.payload[flag]);
+  }
+
+  console.log("flag cache: ", FlagCache);
 })();
 
 // test subscriber
 app.post("/flagUpdates", (req, res) => {
-  flagData.updateFlagData(req.body);
+  handleUpdateNotification(req.body);
 
   // write to SSE stream
 
   // flags logged & sent back for POSTman tests
-  console.log("flag data: ", flagData.formattedForSDK());
-  res.status(200).json({ flags: flagData.formattedForSDK() });
+  console.log("flag data: ", FlagCache);
+  res.status(200).json({ flags: FlagCache });
 });
 
 // route for SDK to fetch routes
 app.get("/sdk/flags", (req, res) => {
-  res.status(200).json({ flags: flagData.formattedForSDK() });
+  res.status(200).json({ payload: FlagCache });
 });
 
 // not used on first iteration
