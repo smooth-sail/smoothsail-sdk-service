e7964084e235263c595187c11a0a1e0217336e84
Emily Olszewski <92195356+emilyoski@users.noreply.github.com>
2023-11-02 15:25:05 -0700

    comment out no longer needed fetch

--- src/controllers/flags.controller.js
+++ src/controllers/flags.controller.js
@@ -1,41 +1,20 @@
-// import EventEmitter from "events";
 import FlagCache from "../cache/flagCache";
-// import Clients from "../models/sse-clients";
 import clients from "../models/sse-clients";
 import jsm from "../nats";
-// import Flag from "../models/Flags";
 
-// let clients = new Clients();
-
-export const getAllFlags = async (req, res) => {
-  try {
-    res.status(200).json({ payload: FlagCache });
-  } catch (err) {
-    res.status(500).json({ error: "Internal error occurred." });
-  }
-};
-
-export const getFlagById = async (req, res) => {
-  const flagId = req.params.id;
-
-  let flag;
-  try {
-    // flag = await pg.getFlag(flagId);
-  } catch (err) {
-    res.status(500).json({ error: "Internal error occurred." });
-  }
-
-  if (!flag) {
-    res.status(404).json({ error: `Flag with id ${flagId} does not exist` });
-  }
-
-  res.status(200).json(flag);
-};
+// fetch no longer needed
+// export const getAllFlags = async (req, res) => {
+//   try {
+//     res.status(200).json({ payload: FlagCache });
+//   } catch (err) {
+//     res.status(500).json({ error: "Internal error occurred." });
+//   }
+// };
 
 export const sseNotifications = async (req, res) => {
   const sdkKey = req.query.key;
   const allowAccess = await jsm.validateSdkKey(sdkKey);
-  console.log("should this key allow access: ", allowAccess);
+  console.log("should this key be allowed access: ", allowAccess);
 
   if (allowAccess["isValid"]) {
     const headers = {
@@ -60,7 +39,7 @@ export const sseNotifications = async (req, res) => {
       console.log(`${clientId} Connection closed`);
       clients.closeClient(clientId);
     });
+  } else {
+    res.status(401).send({ error: "Invalid credentials" });
   }
 };
-
-// router.get('/status', (req, res) => res.json({clients: clients.length})); // tmp route
--- src/routes/Api.routes.js
+++ src/routes/Api.routes.js
@@ -1,25 +1,25 @@
 import express from "express";
 import * as flagsController from "../controllers/flags.controller";
-import { handleUpdateNotification } from "../utils/flags";
-import FlagCache from "../cache/flagCache";
+// import { handleUpdateNotification } from "../utils/flags";
+// import FlagCache from "../cache/flagCache";
 
 const router = express.Router();
 
-// route for SDK to fetch flags
-router.get("/flags", flagsController.getAllFlags);
+// route for SDK to fetch flags - no longer needed
+// router.get("/flags", flagsController.getAllFlags);
 
 // SSE API routes
 router.get("/ff-updates-stream", flagsController.sseNotifications);
 
 // test subscriber
-router.post("/flagUpdates", (req, res) => {
-  handleUpdateNotification(req.body);
+// router.post("/flagUpdates", (req, res) => {
+//   handleUpdateNotification(req.body);
 
-  // write to SSE stream
+//   // write to SSE stream
 
-  // flags logged & sent back for POSTman tests
-  console.log("flag data: ", FlagCache);
-  res.status(200).json({ flags: FlagCache });
-});
+//   // flags logged & sent back for POSTman tests
+//   console.log("flag data: ", FlagCache);
+//   res.status(200).json({ flags: FlagCache });
+// });
 
 export default router;
